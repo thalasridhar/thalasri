dd4084c3d09c04a7bc548f8d233e93bb2b223c95
thalasridhar <30343109+thalasridhar@users.noreply.github.com>
2017-07-21 17:10:20 +0530

    Create  thalasri.c

---
+++ thalasri.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+voidmain()
+{
+int a;
+printf("\n enter the number");
+scanf("%d",&a);
+if(a>0)
+{
+printf("\n number is positive");
+}
+else
+{
+printf("\n number is negative");
+}
+else
+{
+printf("\n number is zero")
+}
+}
